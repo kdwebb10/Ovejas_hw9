0231a8c007bb34790365a1705a00abbf70490f3c
kyle webb <kylewebb4@weber.edu>
2017-04-07 01:59:25 +0000

    File open

--- Overjas_hw9_task2.c
+++ Overjas_hw9_task2.c
@@ -13,12 +13,23 @@
  */
 #include <stdio.h>		/* For Standard I/O */
 #include <stdlib.h>		/* Standard Library */
+#include <math.h>		/* Math Libarary compile with -lm */
 
 /* Function Prototypes */
 
 /* Main Program */
 int main(int argc, char *argv[])
 {
+	FILE *inFile;
+	inFile = fopen("zone1.txt", "r");
+	if(inFile == NULL)
+	{
+		printf("The file was not opened.\n");
+		exit(1);
+	}
+	printf("The file was opened.\n");
+//	while(fscanf(infile, "%d", &d[count]) != EOF)
+
 
 	return 0;
 }
